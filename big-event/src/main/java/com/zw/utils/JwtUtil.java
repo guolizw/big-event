d21d0a1fa3f92ff747c7ecdd78b93f0c95e052a4
zhangwei <101725236+guolizw@users.noreply.github.com>
2024-01-23 16:23:17 +0800

    Update JwtUtil.java

--- big-event/src/main/java/com/zw/utils/JwtUtil.java
+++ big-event/src/main/java/com/zw/utils/JwtUtil.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class JwtUtil {
 
-    private static final String KEY = "zw";
+    private static final String KEY = "";//填写自己想要的密钥
 	
 	//接收业务数据,生成token并返回
     public static String genToken(Map<String, Object> claims) {
